5954bc0371dd9abc0444324e095ab274706baed2
David Enoragbon <david.enoragbon@decagon.dev>
2021-08-03 12:20:55 +0100

    user controllers

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -96,6 +96,16 @@ const userCtrl = {
       return res.status(500).json({ msg: err.message });
     }
   },
+  getUser: async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.user.id).select('-password');
+        if (!user) return res.status(400).json({ msg: 'User does not exist.' });
+
+        res.json(user);
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
 };
 
 const createAccessToken = (user) => {
