769846887f33573ffb33377f60033e99cea20930
David Enoragbon <david.enoragbon@decagon.dev>
2021-08-03 14:23:35 +0100

    product control

--- server/server.js
+++ server/server.js
@@ -20,6 +20,7 @@ app.use(
 app.use('/user', userRouter);
 app.use('/api', require('./routes/categoryRouter'));
 app.use('/api', require('./routes/upload'));
+app.use('/api', require('./routes/productRouter'));
 
 //Connect to mongoDB
 connect();
