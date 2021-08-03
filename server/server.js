25897e70177ca6f9fc3e57c8f88a324bd5906284
David Enoragbon <david.enoragbon@decagon.dev>
2021-08-03 09:31:42 +0100

    first commit

---
+++ server/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+const { connect } = require('./database/mongoDBConnection');
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+//Connect to mongoDB
+connect();
+
+app.get('/', (req, res) => {
+  res.json({ msg: 'Welcome David' });
+});
+
+const PORT = process.env.PORT || '5000';
+app.listen(PORT, () => {
+  console.log('Server is running on port :', PORT);
+});
