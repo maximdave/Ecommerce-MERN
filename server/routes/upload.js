2bdb174b97af9b088dd87f13d05cdf9e78f7cfc7
David Enoragbon <david.enoragbon@decagon.dev>
2021-08-03 14:08:18 +0100

    delete image

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -12,30 +12,36 @@ cloudinary.config({
 });
 
 // Upload image only admin can use
-router.post('/upload', (req, res) => {
+router.post('/upload', auth, authAdmin, (req, res) => {
   try {
-    if(!req.files || Object.keys(req.files).length === 0)
-        return res.status(400).json({msg: 'No files were uploaded.'})
+    if (!req.files || Object.keys(req.files).length === 0)
+      return res.status(400).json({ msg: 'No files were uploaded.' });
 
-        //if file size > 1mb
+    //if file size > 1mb
     const file = req.files.file;
-    if(file.size > 1024*1024) {
-        removeTmp(file.tempFilePath)
-        return res.status(400).json({msg: "Size too large, Should be less than 1 MB"})
+    if (file.size > 1024 * 1024) {
+      removeTmp(file.tempFilePath);
+      return res
+        .status(400)
+        .json({ msg: 'Size too large, Should be less than 1 MB' });
     }
 
-    if(file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png'){
-        removeTmp(file.tempFilePath)
-        return res.status(400).json({msg: "File format is incorrect."})
+    if (file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png') {
+      removeTmp(file.tempFilePath);
+      return res.status(400).json({ msg: 'File format is incorrect.' });
     }
 
-    cloudinary.v2.uploader.upload(file.tempFilePath, {folder: "ecommerce"}, async(err, result)=>{
-        if(err) throw err;
+    cloudinary.v2.uploader.upload(
+      file.tempFilePath,
+      { folder: 'ecommerce' },
+      async (err, result) => {
+        if (err) throw err;
 
-        removeTmp(file.tempFilePath)
+        removeTmp(file.tempFilePath);
 
-        res.json({public_id: result.public_id, url: result.secure_url})
-    })
+        res.json({ public_id: result.public_id, url: result.secure_url });
+      }
+    );
   } catch (err) {
     return res.status(500).json({ msg: err.message });
   }
