bbfcec4b2ba23b810e425818fafd04ee2caefb7a
David Enoragbon <david.enoragbon@decagon.dev>
2021-08-04 11:44:02 +0100

    Login & register

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { createContext, useState, useEffect } from 'react';
 import ProductsAPI from './api/ProductsAPI';
 // import UserAPI from './api/UserAPI'
@@ -9,6 +10,17 @@ export const GlobalState = createContext();
 
 export const DataProvider = ({ children }) => {
   const [token, setToken] = useState(false);
+
+  const refreshToken = async () => {
+    const res = await axios.get('/user/refresh_token');
+
+    setToken(res.data.accesstoken);
+  };
+
+  useEffect(() => {
+    const firstLogin = localStorage.getItem('firstLogin');
+    if (firstLogin) refreshToken();
+  }, []);
 
   const state = {
     token: [token, setToken],
@@ -38,9 +50,5 @@ export const DataProvider = ({ children }) => {
   //     categoriesAPI: CategoriesAPI()
   // }
 
-  return (
-    <GlobalState.Provider value={state}>
-      {children}
-    </GlobalState.Provider>
-  );
+  return <GlobalState.Provider value={state}>{children}</GlobalState.Provider>;
 };
