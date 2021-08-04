506b3e3ab07e3f9bc1a459b2e8f55621e8f1decb
David Enoragbon <david.enoragbon@decagon.dev>
2021-08-04 02:07:52 +0100

    product component

--- client/src/components/mainpages/detailProduct/DetailProduct.js
+++ client/src/components/mainpages/detailProduct/DetailProduct.js
@@ -7,10 +7,23 @@ const DetailProduct = () => {
   const params = useParams();
   const state = useContext(GlobalState);
   const [products] = state.productsAPI.products;
-  const addCart = state.userAPI.addCart;
+  //   const addCart = state.userAPI.addCart;
   const [detailProduct, setDetailProduct] = useState([]);
-  console.log(params);
-  return <div>detail page</div>;
+
+  useEffect(() => {
+    if (params.id) {
+      products.forEach((product) => {
+        if (product._id === params.id) setDetailProduct(product);
+      });
+    }
+  }, [params, products]);
+
+  console.log(detailProduct);
+  return (
+    <div className='detail'>
+      <img src={detailProduct.images.url} alt='' />
+    </div>
+  );
 };
 
 export default DetailProduct;
