0d793f6ecc65263da319be00bc85c458769a343b
David Enoragbon <david.enoragbon@decagon.dev>
2021-08-07 16:35:03 +0100

    Cart

--- client/src/components/mainpages/Pages.js
+++ client/src/components/mainpages/Pages.js
@@ -1,20 +1,25 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Products from './products/Products';
-import DetailProduct from './detailProduct/DetailProduct'
+import DetailProduct from './detailProduct/DetailProduct';
 import Login from './auth/Login';
 import Register from './auth/Register';
 import Cart from './cart/Cart';
 import NotFound from './utils/not_found/NotFound';
+import { GlobalState } from '../../GlobalState';
 
 const Pages = () => {
+  const state = useContext(GlobalState);
+  const [isLogged] = state.userAPI.isLogged;
+  const [isAdmin] = state.userAPI.isAdmin;
+
   return (
     <Switch>
       <Route path='/' exact component={Products} />
-      <Route path="/detail/:id" exact component={DetailProduct} />
+      <Route path='/detail/:id' exact component={DetailProduct} />
 
-      <Route path='/login' exact component={Login} />
-      <Route path='/register' exact component={Register} />
+      <Route path='/login' exact component={isLogged ? NotFound : Login} />
+      <Route path='/register' exact component={isLogged ? NotFound : Register} />
 
       {/* <Route path="/category" exact component={isAdmin ? Categories : NotFound} />
             <Route path="/create_product" exact component={isAdmin ? CreateProduct : NotFound} />
--- client/src/components/mainpages/cart/Cart.js
+++ client/src/components/mainpages/cart/Cart.js
@@ -1,13 +1,71 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { GlobalState } from '../../../GlobalState';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 const Cart = () => {
   const state = useContext(GlobalState);
-  //   const [cart, setCart] = state.userAPI.cart;
-  const [total, setTotal] = useState(0)
-  const [cart] = state.userAPI.cart;
+  const [total, setTotal] = useState(0);
+  const [token] = state.token;
+
+  const [cart, setCart] = state.userAPI.cart;
+
+  useEffect(() => {
+    const getTotal = () => {
+      const total = cart.reduce((prev, item) => {
+        return prev + item.price * item.quantity;
+      }, 0);
+
+      setTotal(total);
+    };
+
+    getTotal();
+  }, [cart]);
+
+  const addToCart = async (cart) => {
+    await axios.patch(
+      '/user/addcart',
+      { cart },
+      {
+        headers: { Authorization: token },
+      }
+    );
+  };
+
+  const increment = (id) => {
+    cart.forEach((item) => {
+      if (item._id === id) {
+        item.quantity += 1;
+      }
+    });
+
+    setCart([...cart]);
+    addToCart(cart);
+  };
+
+  const decrement = (id) => {
+    cart.forEach((item) => {
+      if (item._id === id) {
+        item.quantity === 1 ? (item.quantity = 1) : (item.quantity -= 1);
+      }
+    });
+
+    setCart([...cart]);
+    addToCart(cart);
+  };
+
+  const removeProduct = (id) => {
+    if (window.confirm('Do you want to delete this product?')) {
+      cart.forEach((item, index) => {
+        if (item._id === id) {
+          cart.splice(index, 1);
+        }
+      });
+
+      setCart([...cart]);
+        addToCart(cart);
+    }
+  };
 
   if (cart.length === 0)
     return (
@@ -17,22 +75,23 @@ const Cart = () => {
   return (
     <div>
       {cart.map((product) => (
-        <div className='detail cart'>
-          <img src={product.images.url} alt=''/>
+        <div className='detail cart' key={product._id}>
+          <img src={product.images.url} alt='' />
           <div className='box-detail'>
             <h2>{product.title}</h2>
-            {/* <h6>#id: {product.product_id}</h6> */}
             <h3>$ {product.price * product.quantity}</h3>
             <p>{product.description}</p>
             <p>{product.content}</p>
 
             <div className='amount'>
-              <button>-</button>
+              <button onClick={() => decrement(product._id)}>-</button>
               <span>{product.quantity}</span>
-              <button>+</button>
+              <button onClick={() => increment(product._id)}>+</button>
             </div>
 
-            <div className='delete'>X</div>
+            <div className='delete' onClick={() => removeProduct(product._id)}>
+              X
+            </div>
           </div>
         </div>
       ))}
